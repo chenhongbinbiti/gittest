e7bd7e4922283bb0f0211908e56bba9c723020da
hbchen <101390731@qq.com>
2017-01-08 20:34:45 +0800

    wangzhi

--- myscrapy/spiders/qiubai.py
+++ myscrapy/spiders/qiubai.py
@@ -1,7 +1,7 @@
 import scrapy
 class Qiubai(scrapy.Spider):
     name = "qiubai"
-    start_url = ["http://qiushibaike.com",]
+    start_url = ["http://www.qiushibaike.com",]
     def parse(self, response):
         print response.xpath('//div[@class="content"]').extract()
 
